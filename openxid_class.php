6a50b42dcecaa3c55f8fc234227519565c8b985a
mib <mib@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-06-30 14:24:09 +0000

    Erstattet tabs med spaces
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/OpenXId/trunk@73755 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- openxid_class.php
+++ openxid_class.php
@@ -30,14 +30,15 @@ class openXId extends webServiceServer {
   protected $solr_server = "";
   protected $result_fields = "";
   protected $searchcode = array();
+      
 
   function __construct($inifile, $silent=false) {
     parent::__construct($inifile);
     verbose::log(DEBUG, "ini " . $inifile );
-		$this->solr_server = $this->config->get_value( "solr_server", "solr" );
-		$this->searchcode = $this->config->get_value( "searchcode", "solr" );
-		$this->result_fields = $this->config->get_value( "result_set", "solr" );
-		$this->curl = new curl();
+    $this->solr_server = $this->config->get_value( "solr_server", "solr" );
+    $this->searchcode = $this->config->get_value( "searchcode", "solr" );
+    $this->result_fields = $this->config->get_value( "result_set", "solr" );
+    $this->curl = new curl();
     verbose::log(TRACE, "openxid:: openxid initialized");
 
   }
@@ -90,13 +91,13 @@ class openXId extends webServiceServer {
   *
   */
   protected function do_search($search_string, $result_fields ) {
-	  $url_append = "/select?q=" . $search_string . "&rows=999&fl=" . $result_fields . "&wt=phps&defType=edismax&stopwords=true&lowercaseOperators=true" ;
-	  verbose::log(DEBUG, 'URL ' . $this->solr_server . $url_append );
-	  $get_result = $this->curl->get( $this->solr_server . $url_append );
-	  $result_array[] = @ unserialize( $get_result );
-	  verbose::log(DEBUG, 'RES <' . $get_result . ">" );
-		return $result_array;
-	}
+    $url_append = "/select?q=" . $search_string . "&rows=999&fl=" . $result_fields . "&wt=phps&defType=edismax&stopwords=true&lowercaseOperators=true" ;
+    verbose::log(DEBUG, 'URL ' . $this->solr_server . $url_append );
+    $get_result = $this->curl->get( $this->solr_server . $url_append );
+    $result_array[] = @ unserialize( $get_result );
+    verbose::log(DEBUG, 'RES <' . $get_result . ">" );
+    return $result_array;
+  }
 // =============================================================================
 
 
@@ -112,18 +113,18 @@ class openXId extends webServiceServer {
       $xid_error = &$xid_getIdsResponse->_value->error;
       $xid_error->_value = "authentication error";
       $xid_error->_namespace = $this->xmlns['xid'];
-			verbose::log(ERROR, "openxid:: getIdsRequest(...); - authentication error");
+      verbose::log(ERROR, "openxid:: getIdsRequest(...); - authentication error");
       return $ret;
     }
 
-		verbose::log(ERROR, "WUT <" . print_r($param, true) . '>' );
+    verbose::log(ERROR, "WUT <" . print_r($param, true) . '>' );
     $paramId = is_array($param->id) ? $param->id : array($param->id);
-		verbose::log(ERROR, "WAT <" . print_r($paramId, true) . '>' );
+    verbose::log(ERROR, "WAT <" . print_r($paramId, true) . '>' );
     if (empty($paramId)) {
       $xid_error = &$xid_getIdsResponse->_value->error;
       $xid_error->_value = "invalid id";
       $xid_error->_namespace = $this->xmlns['xid'];
-			verbose::log(ERROR, "openxid:: getIdsRequest(...); - invalid request <" . print_r($param, true) . '>' );
+      verbose::log(ERROR, "openxid:: getIdsRequest(...); - invalid request <" . print_r($param, true) . '>' );
       return $ret;
     }
 
@@ -134,7 +135,7 @@ class openXId extends webServiceServer {
       $item['idValue'] = strip_tags($id->_value->idValue->_value);
       // check if idType is supported
       if (!array_key_exists($item['idType'], $this->searchcode)) {
-				verbose::log(ERROR, "invalid idType in request <" . $item['idType'] . '>');
+        verbose::log(ERROR, "invalid idType in request <" . $item['idType'] . '>');
         $item['error'] = "invalid idType";
         $clusterData[] = $item;
         continue;  // Next iteration
@@ -142,130 +143,130 @@ class openXId extends webServiceServer {
       // Normalize
       $idValue = self::_normalize($item['idType'], $item['idValue']);
       if ($idValue === 0) {
-				verbose::log(ERROR, "invalid id in request <" . $item['idValue'] . '>');
+        verbose::log(ERROR, "invalid id in request <" . $item['idValue'] . '>');
         $item['error'] = "invalid id"; 
         $clusterData[] = $item;
         continue;  // Next iteration
       }
 
-			verbose::log(TRACE, 'REQUEST type <' . $this->searchcode[ $item['idType'] ] . "> value <" . $idValue . ">" );
-			$search_string = "" . $this->searchcode[ $item ['idType'] ] . "%3A" . $idValue ;
-			verbose::log(TRACE, 'SEARCH ' . $search_string );
-			$result_array = self::do_search($search_string, "unit.id" );
-			verbose::log(DEBUG, 'UNITID_RESULT ' . print_r($result_array, true) );
+      verbose::log(TRACE, 'REQUEST type <' . $this->searchcode[ $item['idType'] ] . "> value <" . $idValue . ">" );
+      $search_string = "" . $this->searchcode[ $item ['idType'] ] . "%3A" . $idValue ;
+      verbose::log(TRACE, 'SEARCH ' . $search_string );
+      $result_array = self::do_search($search_string, "unit.id" );
+      verbose::log(DEBUG, 'UNITID_RESULT ' . print_r($result_array, true) );
 
 
-			// Uniqing unit.id's
-			$num_of_rows_found = $result_array[0]['response']['numFound'];
-			if ( $num_of_rows_found > 999 ) {
-				verbose::log(WARNING, 'More than 999 unit.id hits on ' . $search_string . ' (got : ' . $num_of_rows_found . ") rest is ignored");
-			}
-			$unit_ids = array();
+      // Uniqing unit.id's
+      $num_of_rows_found = $result_array[0]['response']['numFound'];
+      if ( $num_of_rows_found > 999 ) {
+        verbose::log(WARNING, 'More than 999 unit.id hits on ' . $search_string . ' (got : ' . $num_of_rows_found . ") rest is ignored");
+      }
+      $unit_ids = array();
 
-			$docs = array();
-			$docs = $result_array[0]['response']['docs'];
-			foreach ( $docs as $tempodocs ) {
-				if ( ! isset( $unit_ids[ $tempodocs['unit.id' ] ] ) ) {
-					// search string is "unit.id:unit:<unit.id>" and ['unit.id'] contains "unit:<unit.id>"
-					$unit_ids[ $tempodocs['unit.id' ] ] = substr_replace( $tempodocs['unit.id' ], ".id%3Aunit%5C%3A", 4, 1);
-				}
-			}
+      $docs = array();
+      $docs = $result_array[0]['response']['docs'];
+      foreach ( $docs as $tempodocs ) {
+        if ( ! isset( $unit_ids[ $tempodocs['unit.id' ] ] ) ) {
+          // search string is "unit.id:unit:<unit.id>" and ['unit.id'] contains "unit:<unit.id>"
+          $unit_ids[ $tempodocs['unit.id' ] ] = substr_replace( $tempodocs['unit.id' ], ".id%3Aunit%5C%3A", 4, 1);
+        }
+      }
 
-			// For each unit.id found in previous search we now have to find the wanted ids of 
-			// various types.
-			$newClusterData = array();
-			foreach ( $unit_ids as $tempo ) {
-				$result_array = self::do_search($tempo, $this->result_fields);
-				verbose::log(DEBUG, 'TERMS_RESULT ' . print_r($result_array, true) );
+      // For each unit.id found in previous search we now have to find the wanted ids of 
+      // various types.
+      $newClusterData = array();
+      foreach ( $unit_ids as $tempo ) {
+        $result_array = self::do_search($tempo, $this->result_fields);
+        verbose::log(DEBUG, 'TERMS_RESULT ' . print_r($result_array, true) );
 
-				$docs = array();
-				$docs = $result_array[0]['response']['docs'];
-				foreach ( $docs as $tempodoc ) {
-					$recarr = array();
+        $docs = array();
+        $docs = $result_array[0]['response']['docs'];
+        foreach ( $docs as $tempodoc ) {
+          $recarr = array();
 
-					if ( isset( $tempodoc['dkcclterm.id'] ) ) {
-						// 001 *a
-						$recarr = $tempodoc['dkcclterm.id'];
-						foreach ( $recarr as $temporecid ) {
-							if ( strpos( $temporecid, '_') > 0 ) {
-								continue;
-							}
-							if (is_array( $tempodoc['dkcclterm.ln'] ) ) {
-								$tmpbib = $tempodoc['dkcclterm.ln'][0];
-							} else {
-								$tmpbib = $tempodoc['dkcclterm.ln'];
-							}
-							if ($tmpbib >= 870970 and $tmpbib <= 870979 ) {
-								// There are a few records from these bases with invalid faust - this fixes that problem
-								if ( materialId::normalizeFaust($temporecid) ) {
-									$xxx['idtype'] = 'faust';
-									$xxx['idvalue'] =  materialId::normalizeFaust($temporecid);
-								} else {
-									$xxx['idtype'] = 'local';
-									$xxx['idvalue'] =  $temporecid;
-								}
-							} else {
-								$xxx['idtype'] = 'local';
-								$xxx['idvalue'] =  $temporecid;
-							}
-							$newClusterData[] = $xxx;
-						}
-					}
-					// 002 *acd
-					if ( isset( $tempodoc['dkcclterm.tf'] ) ) {
-						$recarr = $tempodoc['dkcclterm.tf'];
-						foreach ( $recarr as $temporecid ) {
-							// content of *d isn't always trustworthy as a faust
-							if ( materialId::normalizeFaust($temporecid) ) {
-								$xxx['idtype'] = 'faust';
-								$xxx['idvalue'] =  materialId::normalizeFaust($temporecid);
-							} else {
-								$xxx['idtype'] = 'local';
-								$xxx['idvalue'] =  $temporecid;
-							}
-							$newClusterData[] = $xxx;
-						}
-					}
-					// isbn
-					if ( isset( $tempodoc['dkcclterm.ib'] ) ) {
-						$recarr = $tempodoc['dkcclterm.ib'];
-						foreach ( $recarr as $temporecid ) {
-							$tmpstr = self::_normalize( 'ean', $temporecid );
-							switch ( strlen( $tmpstr ) ) {
-								case 10:
-									$xxx['idtype'] = 'isbn';
-									break;
-								case 13:
-									$xxx['idtype'] = 'ean';
-									break;
-								default:
-									verbose::log(WARNING, 'Neither isbn nor ean in ib section <' . print_r($temporecid, true) . '>' );
-									continue 2;
-									break;
-							}
-							$xxx['idvalue'] =  $tmpstr;
-							$newClusterData[] = $xxx;
-						}
-					}
-					// issn
-					if ( isset( $tempodoc['dkcclterm.in'] ) ) {
-						$recarr = $tempodoc['dkcclterm.in'];
-						foreach ( $recarr as $temporecid ) {
-							$tmpstr =  materialId::normalizeISSN($temporecid);
-							if ( strlen( $tmpstr ) == 8 ) {
-								$xxx['idtype'] = 'issn';
-								$xxx['idvalue'] =  $tmpstr;
-								$newClusterData[] = $xxx;
-							} else {
-								// This message can end up being pretty annoying because isbn also occurs here so potential isbn/ean are ignored.
-								if ( ! (strlen( $tmpstr ) == 10 || strlen( $tmpstr ) == 13) ) {
-									verbose::log(WARNING, 'Not an issn value in is section <' . print_r($temporecid, true) . '>' );
-								}
-							}
-						}
-					}
-				}
-			}
+          if ( isset( $tempodoc['dkcclterm.id'] ) ) {
+            // 001 *a
+            $recarr = $tempodoc['dkcclterm.id'];
+            foreach ( $recarr as $temporecid ) {
+              if ( strpos( $temporecid, '_') > 0 ) {
+                continue;
+              }
+              if (is_array( $tempodoc['dkcclterm.ln'] ) ) {
+                $tmpbib = $tempodoc['dkcclterm.ln'][0];
+              } else {
+                $tmpbib = $tempodoc['dkcclterm.ln'];
+              }
+              if ($tmpbib >= 870970 and $tmpbib <= 870979 ) {
+                // There are a few records from these bases with invalid faust - this fixes that problem
+                if ( materialId::normalizeFaust($temporecid) ) {
+                  $xxx['idtype'] = 'faust';
+                  $xxx['idvalue'] =  materialId::normalizeFaust($temporecid);
+                } else {
+                  $xxx['idtype'] = 'local';
+                  $xxx['idvalue'] =  $temporecid;
+                }
+              } else {
+                $xxx['idtype'] = 'local';
+                $xxx['idvalue'] =  $temporecid;
+              }
+              $newClusterData[] = $xxx;
+            }
+          }
+          // 002 *acd
+          if ( isset( $tempodoc['dkcclterm.tf'] ) ) {
+            $recarr = $tempodoc['dkcclterm.tf'];
+            foreach ( $recarr as $temporecid ) {
+              // content of *d isn't always trustworthy as a faust
+              if ( materialId::normalizeFaust($temporecid) ) {
+                $xxx['idtype'] = 'faust';
+                $xxx['idvalue'] =  materialId::normalizeFaust($temporecid);
+              } else {
+                $xxx['idtype'] = 'local';
+                $xxx['idvalue'] =  $temporecid;
+              }
+              $newClusterData[] = $xxx;
+            }
+          }
+          // isbn
+          if ( isset( $tempodoc['dkcclterm.ib'] ) ) {
+            $recarr = $tempodoc['dkcclterm.ib'];
+            foreach ( $recarr as $temporecid ) {
+              $tmpstr = self::_normalize( 'ean', $temporecid );
+              switch ( strlen( $tmpstr ) ) {
+                case 10:
+                  $xxx['idtype'] = 'isbn';
+                  break;
+                case 13:
+                  $xxx['idtype'] = 'ean';
+                  break;
+                default:
+                  verbose::log(WARNING, 'Neither isbn nor ean in ib section <' . print_r($temporecid, true) . '>' );
+                  continue 2;
+                  break;
+              }
+              $xxx['idvalue'] =  $tmpstr;
+              $newClusterData[] = $xxx;
+            }
+          }
+          // issn
+          if ( isset( $tempodoc['dkcclterm.in'] ) ) {
+            $recarr = $tempodoc['dkcclterm.in'];
+            foreach ( $recarr as $temporecid ) {
+              $tmpstr =  materialId::normalizeISSN($temporecid);
+              if ( strlen( $tmpstr ) == 8 ) {
+                $xxx['idtype'] = 'issn';
+                $xxx['idvalue'] =  $tmpstr;
+                $newClusterData[] = $xxx;
+              } else {
+                // This message can end up being pretty annoying because isbn also occurs here so potential isbn/ean are ignored.
+                if ( ! (strlen( $tmpstr ) == 10 || strlen( $tmpstr ) == 13) ) {
+                  verbose::log(WARNING, 'Not an issn value in is section <' . print_r($temporecid, true) . '>' );
+                }
+              }
+            }
+          }
+        }
+      }
 
       if (!is_array($newClusterData) or (count($newClusterData)==0)) {
         $item['error'] = "no results found for requested id"; 
@@ -322,14 +323,14 @@ class openXId extends webServiceServer {
   *
   */
   function updateIdRequest($param) {
-		verbose::log(DEBUG, "openxid:: updateIdRequest(...);");
-		$xid_updateIdResponse = &$ret->updateIdResponse;
-		$xid_updateIdResponse->_namespace = $this->xmlns['xid'];
-		$xid_error = &$xid_updateIdResponse->_value->error;
-		$xid_error->_value = "Update is not possible when using Solr";
-		$xid_error->_namespace = $this->xmlns['xid'];
-		return $ret;
-	}
+    verbose::log(DEBUG, "openxid:: updateIdRequest(...);");
+    $xid_updateIdResponse = &$ret->updateIdResponse;
+    $xid_updateIdResponse->_namespace = $this->xmlns['xid'];
+    $xid_error = &$xid_updateIdResponse->_value->error;
+    $xid_error->_value = "Update is not possible when using Solr";
+    $xid_error->_namespace = $this->xmlns['xid'];
+    return $ret;
+  }
 
 }
 
@@ -338,7 +339,7 @@ class openXId extends webServiceServer {
 //* tab-width: 2
 //* c-basic-offset: 2
 //* End:
-//* vim600: sw=2 ts=2 fdm=marker
-//* vim<600: sw=2 ts=2
+//* vim600: sw=2 ts=2 fdm=marker expandtab
+//* vim<600: sw=2 ts=2 expandtab
 //*/
 ?>
